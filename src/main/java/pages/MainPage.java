b961f1254409f3e5f559be18ae9492fb405e4f7e
wojtek <wojciech3a@interia.pl>
2021-07-17 10:27:31 +0200

    Modified MainPage

--- src/main/java/pages/MainPage.java
+++ src/main/java/pages/MainPage.java
@@ -2,10 +2,16 @@ package pages;
 
 import base.SeleniumBasePage;
 import helpers.GlobalMethods;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.testng.reporters.jq.Main;
+
+import java.security.Key;
 
 public class MainPage extends SeleniumBasePage {
 
@@ -25,7 +31,7 @@ public class MainPage extends SeleniumBasePage {
     @FindBy(id = "newsletter_surname")
     private WebElement fldSurname;
 
-    @FindBy(css = "div[class*='ant-select-dropdown']")
+    @FindBy(css = "ul[class*='ant-select-dropdown-menu']")
     private WebElement dpdNewsletterType;
 
     @FindBy(css = "div[class='ant-calendar-input-wrap']")
@@ -44,12 +50,45 @@ public class MainPage extends SeleniumBasePage {
     private WebElement btnSubmit;
 
 
-    public MainPage initialTest(){
-        globalMethods.setInput(fldEmail, "first test");
+    public MainPage checkSetDropdown(){
+        WebElement elem = driver.findElement(By.id("newsletter_newsType"));
+        Actions builder = new Actions(driver);
+        wait.until(ExpectedConditions.elementToBeClickable(elem));
+        builder.sendKeys(elem,Keys.DOWN).build().perform();
+        globalMethods.setDropdown(dpdNewsletterType, "Medical");
+
         return this;
     }
 
+    public void setInputForEmailField(String txt){
+        globalMethods.setInput(fldEmail,txt);
+    }
 
+    public String getInputFromEmailField(String txt){
+        return globalMethods.getTextFromElement(fldEmail);
+    }
 
+    public void setInputForNameField(String txt){
+        globalMethods.setInput(fldFirstName,txt);
+
+    }
+
+    public String getInputFromNameField(String txt){
+        return globalMethods.getTextFromElement(fldFirstName);
+    }
+
+    public void setInputForSurnameField(String txt){
+        globalMethods.setInput(fldSurname,txt);
+
+    }
+
+    public String getInputFromSurnameField(String txt){
+        return globalMethods.getTextFromElement(fldSurname);
+
+    }
+
+    public void setInputForNewsletterType(String txt){
+        globalMethods.setDropdown(dpdNewsletterType,txt);
+    }
 
 }
