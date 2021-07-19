28c13c17ee8f72aa3b553d7f9718d3ef9ab71c60
wojtek <wojciech3a@interia.pl>
2021-07-19 20:05:49 +0200

    Added new methods to mainPage

--- src/main/java/pages/MainPage.java
+++ src/main/java/pages/MainPage.java
@@ -2,19 +2,15 @@ package pages;
 
 import base.SeleniumBasePage;
 import helpers.GlobalMethods;
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
 import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class MainPage extends SeleniumBasePage {
 
     GlobalMethods globalMethods;
 
-    public MainPage(WebDriver driver){
+    public MainPage(WebDriver driver) {
         super(driver);
         globalMethods = new GlobalMethods(driver);
     }
@@ -47,72 +43,102 @@ public class MainPage extends SeleniumBasePage {
     private WebElement btnSubmit;
 
 
-    public void setInputForEmailField(String txt){
-        globalMethods.setInput(fldEmail,txt);
-    }
+    public MainPage setInputForEmailField(String txt) {
+        globalMethods.setInput(fldEmail, txt);
 
-    public String getInputFromEmailField(){
-        return globalMethods.getTextFromInputElement(fldEmail);
-    }
-
-    public void setInputForNameField(String txt){
-        globalMethods.setInput(fldFirstName,txt);
-
-    }
-
-    public String getInputFromNameField(){
-        return globalMethods.getTextFromInputElement(fldFirstName);
-    }
-
-    public void setInputForSurnameField(String txt){
-        globalMethods.setInput(fldSurname,txt);
-
-    }
-
-    public String getInputFromSurnameField(){
-        return globalMethods.getTextFromInputElement(fldSurname);
-
-    }
-
-    public MainPage setInputForNewsletterType(String txt){
-        globalMethods.setDropdown(dpdNewsletterType,txt);
         return this;
     }
 
-    public String getOptionFromNewsletterDropdown(){
+    public String getInputFromEmailField() {
+        return globalMethods.getTextFromInputElement(fldEmail);
+    }
+
+    public MainPage setInputForNameField(String txt) {
+        globalMethods.setInput(fldFirstName, txt);
+
+        return this;
+    }
+
+    public String getInputFromNameField() {
+        return globalMethods.getTextFromInputElement(fldFirstName);
+    }
+
+    public MainPage setInputForSurnameField(String txt) {
+        globalMethods.setInput(fldSurname, txt);
+
+        return this;
+    }
+
+    public String getInputFromSurnameField() {
+        return globalMethods.getTextFromInputElement(fldSurname);
+    }
+
+    public MainPage setInputForNewsletterType(String txt) {
+        globalMethods.setDropdown(dpdNewsletterType, txt);
+        return this;
+    }
+
+    public String getOptionFromNewsletterDropdown() {
         return globalMethods.getTextFromDropdown(dpdNewsletterType);
     }
 
-    public void setCalendarStartingDate(String date){
-        globalMethods.setCalendarDate(dtpStartingDate,date);
+    public MainPage setCalendarStartingDate(String date) {
+        globalMethods.setCalendarDate(dtpStartingDate, date);
+
+        return this;
     }
 
-    public String getCalendarStartingDate(){
+    public String getCalendarStartingDate() {
         return globalMethods.getDateFromCalendar(dtpStartingDate);
     }
 
-    public void setCalendarEndDate(String date){
-        globalMethods.setCalendarDate(dtpEndingDate,date);
+    public MainPage setCalendarEndDate(String date) {
+        globalMethods.setCalendarDate(dtpEndingDate, date);
+
+        return this;
     }
 
-    public String getCalendarEndingDate(){
+    public String getCalendarEndingDate() {
         return globalMethods.getDateFromCalendar(dtpEndingDate);
     }
 
-    public void setSexRadiobutton(String choice){
+    public MainPage setSexRadiobutton(String choice) {
         globalMethods.setRadioButton(rdoSex, choice);
+
+        return this;
     }
 
-    public String getSexRadiobuttonValue(){
+    public String getSexRadiobuttonValue() {
         return globalMethods.getOptionFromRadiobutton(rdoSex);
     }
 
-    public void clickCheckbox(){
+    public MainPage clickCheckbox() {
         globalMethods.setCheckboxElement(chkNewsletterAgreement);
+
+        return this;
     }
 
-    public boolean getCheckboxState(){
-       return globalMethods.getChechboxElementState(chkNewsletterAgreement);
+    public boolean getCheckboxState() {
+        return globalMethods.getCheckboxElementState(chkNewsletterAgreement);
     }
+
+    public MainPage clickOnSubmitButton() {
+        globalMethods.clickOnButton(btnSubmit);
+
+        return this;
+    }
+
+    public MainPage scrollToSubmitButton() {
+        globalMethods.scrollToElement(btnSubmit);
+
+        return this;
+    }
+
+    public MainPage closeConfirmDialog() {
+        btnSubmit.click();
+
+        return this;
+    }
+
 
 }
